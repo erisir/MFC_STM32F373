cbcc5bfaab6108cbc8be9feead98a511f4ecdb51
erisir <n.daguan@gmail.com>
2019-08-18 14:47:48 +0800

    add include into project for prePorting freemodbus

---
+++ MFC_RTOS_Hal/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'MFC_RTOS_Hal' 
+ * Target:  'MFC_RTOS_Hal' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
